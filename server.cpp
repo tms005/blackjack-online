10f6c47ce90fba97f20564a368ab9f9ab35c44dd
karol.zaleski87 <karol.zaleski87@gmail.com>
2013-01-21 20:37:16 +0000

    WERSJA KOMPILOWALNA, ALE NIC PONADTO! Zaraz zaczynam ogarniać na nowo kod po odbugowaniu większości funkcji. UWAGA: SPODZIEWAJCIE SIĘ ZMIAN ZNACZĄCYCH W README.TXT!!!

--- server.cpp
+++ server.cpp
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <cstdlib>
+
 #pragma comment (lib, "Ws2_32.lib")
 // #pragma comment (lib, "Mswsock.lib")
 #define DEFAULT_BUFLEN 512
@@ -15,22 +17,17 @@
 
 
 using namespace std;
-
 typedef struct klient
 {
     HANDLE thread;
     SOCKET sock;
 	int ID;
 	int IT;
-} gracze[MAX_CLIENTS];
+};
+klient gracze[MAX_CLIENTS];
 
 
 
-
-///////////////////////////////////////////
-////////////////////////////////////////FUNKCJE:
-void sbMssgCLEAR(); //czyszcze ja
- void sConversion(Buffer *sbMssg, char cMessage[]);
 
 //////////////////////////////////////////////
 ////////////////////////////////ZMIENNE GRY:
@@ -45,20 +42,32 @@ int iPlayerBid[4]; // tutaj obstawiana kasa przez g
 int iaPlayerHand[4][12]; // tutaj karty gracza
 int iActivePlayer; // gracz ktory wlasnie w kolejce podejmuje decyzje
 int iNextPlayer; // kolejny gracz ktory podejmie decyzje
-} 
-STOLIK[MAX_TABLES]; // istnieja 3 stoliki
+} ;
+table STOLIK[MAX_TABLES]; // istnieja 3 stoliki
 
 //struktura wiadomosci
 typedef struct Buffer {
 short ID; //identyfikator funkcji , patrz dalej dostepne klucze
-short ID_USR; // nadawany przez serwer klucz dla ka¿dego po³¹czonego z serwerem u¿ytkownika
+short ID_USR; // nadawany przez serwer klucz dla kaÅ¼dego poÅ‚Ä…czonego z serwerem uÅ¼ytkownika
 short iKey[16]; // w tym polu mamy kolejne argumenty dla funkcji 
 char cChat[256];
 };
+
 int iZalogowany[MAX_CLIENTS]; // tablica zalogowanych graczy
 int iUzytyStol[4];							//tablica wykorzystanych stolow
 
 
+///////////////////////////////////////////
+////////////////////////////////////////FUNKCJE:
+void sbMssgCLEAR(Buffer *sbMssg); 
+ void sConversion(Buffer *sbMssg, char cMessage[]);
+ void rConversion(Buffer *sbMssg, char *cMessage);
+ void cMessageForward(Buffer *sbMssg);
+short iGeneruIdGracza(void);
+int iPorownajHaslo(Buffer *sbMssg, char napis[]);
+char* sWybierzHaslo(Buffer* sbMssg);
+int iTranslate(Buffer* sbMssg);
+///////////////////////////////////////////////////////////////
 	DWORD WINAPI iSilnikGry(LPVOID ctx)
 {
 	int thread_ID =-1; //  tutaj przechowujemy index gracza ktory wywolal watek serwera (index w tablicy gracze)
@@ -73,7 +82,7 @@ int iUzytyStol[4];							//tablica wykorzystanych stolow
 	while(iAktywny)
 	{
 	  ret = recv( gracz->sock, cMessage, 257, 0);
-	   rConversion(&sbMssg, &cMessage);  // za kazdym odebraniem wiadomosci z klienta tlumacze ja z powrotem do postaci ramki struktury
+	   rConversion(&sbMssg, cMessage);  // za kazdym odebraniem wiadomosci z klienta tlumacze ja z powrotem do postaci ramki struktury
 	   if(ret == 0)
 		{
 	   //polaczenie zamkniete po stronie klienta
@@ -92,40 +101,46 @@ int iUzytyStol[4];							//tablica wykorzystanych stolow
 			}
 			return 0;
 	   }
-	iMode=	Translate(sbMssg); // tutaj decydujemy co bedzie sie dzialo ;d
+	iMode=	iTranslate(&sbMssg); // tutaj decydujemy co bedzie sie dzialo ;d
 	
 
 	//tutaj : glowna decyzja serwera .
 	switch(iMode)
 		{
 	case 0: // tutaj wysylamy wiadomosc do swojego stolika lub do wszystkich
-			sbMssgCLEAR();
-			cMessageForward(); // tutaj modyfikacja nieznaczna jedynie potrzebna do dalszego rozeslania (do zaimplementowania)
-			for(int i=0;i<MAX_CLIENTS;i++) if( iTable == gracze[i].IT ) send(gracze[i].sock  , cMessage, 275, 0);
+			sbMssgCLEAR(&sbMssg);
+			cMessageForward(&sbMssg); // tutaj modyfikacja nieznaczna jedynie potrzebna do dalszego rozeslania (do zaimplementowania)
+			for(int i=0;i<MAX_CLIENTS;i++) if( iTable == gracze[i].IT ) send( gracze[i].sock  , cMessage, 513, 0);
 			break;
 
 	case 1:
-		
+		{
 		FILE* login= fopen("login.txt", "wt+");
-		if (login==NULL) {	fputs("File error",stderr); exit (1);} 
+		if (login==NULL) 
+			{fputs("File error",stderr); exit (1);} 
 		 fseek (login , 0 , SEEK_END);
-		sbMssg.ID_USR=generuj_nowe_id_gracza();
-		fprintf(login, "%d %s\n",sbMssg.ID_USR, wybierzHaslo());
+		sbMssg.ID_USR = iGeneruIdGracza();
+		fprintf(login, "%d %s\n",sbMssg.ID_USR, sWybierzHaslo(&sbMssg)); // tutaj do wymiany
 		fclose(login);
-				 sConversion(&sbMssg, cMessage);	
-			   send(gracze[i].sock  , cMessage, 275, 0);
+		sConversion(&sbMssg, cMessage);	
+		send(gracz->sock  , cMessage, 513, 0);
 		break;
-	case 2:{
+
+
+		}
+
+	case 2:
+		{
 		int wolny=0;
 		for(int i=0;i<MAX_CLIENTS;i++) if(iZalogowany[i]==0) wolny=i;
 
-		if(-1==thread_ID && i ){
+		if(-1==thread_ID && wolny ){
 		int k;
-		char napis[275];
-		FILE* login= fopen("login.txt", rt);
+		char napis[512];
+		FILE* login= fopen("login.txt", "rt");
 		 if (login==NULL) {fputs ("File error",stderr); exit (1);}
 		 while( fscanf(login, "%d", &k) && fscanf(login, "%s", &napis) )  if(k ==sbMssg.ID) break;
-		 if(k ==wybierzHaslo())  
+		 if( iPorownajHaslo(&sbMssg, napis))  
 		 {
 			 //zalogowanie poprawne
 			 gracze[wolny].ID=sbMssg.ID;
@@ -146,7 +161,7 @@ int iUzytyStol[4];							//tablica wykorzystanych stolow
 	   {
 		  sbMssg.ID=2; // brak wolnych slotow na serwerze
 		 sConversion(&sbMssg, cMessage);	
-		  send(gracze[thread_ID].sock  , cMessage, 275, 0)
+		  send(gracze[thread_ID].sock  , cMessage, 275, 0);
 
 		}
 	  // tutaj sie wylogowujemy, wiec odsylamy po prostu co dostalismy
@@ -175,8 +190,8 @@ int iUzytyStol[4];							//tablica wykorzystanych stolow
 		sbMssg.iKey[1] = t[2];
 		sbMssg.iKey[2] = t[3];
 
-		for(int i=0;i<MAX_TABLE;i++)
-			for(int j=0;j++;j<4)
+		for(int i=0;i<MAX_TABLES;i++)
+			for(int j=0;j<4;j++)
 			
 		sbMssg.iKey[(i+1)*(j+1) + 3] = STOLIK[i].ID[j]; // tutaj uzupelniam sbMssg (zakladajac ze robie to dobrze xD)s
 		}
@@ -201,13 +216,13 @@ int iUzytyStol[4];							//tablica wykorzystanych stolow
 				iTable=sbMssg.ID_USR;
 				gracz->IT=sbMssg.ID_USR;
 				//uzupelniam sbMssg danymi dla pozostalych graczy i rozsylam:
-				sbMssgCLEAR();
+				sbMssgCLEAR(&sbMssg);
 				
 				sbMssg.ID_USR=gracz->ID;
-				for(int i=0;i<MAX_CLIENTS;i++) if(gracze[i].ID!=gracz->ID) if(gracze[i].IT = iTable)
+				for(int i=0;i<MAX_CLIENTS;i++) if(gracze[i].ID != gracz->ID) if(gracze[i].IT == iTable)
 				{
 				sbMssg.ID = gracze[i].ID;
-				sConverse(sbMssg, cMessage);
+				sConversion(&sbMssg, cMessage);
 				send(gracze[i].sock  , cMessage, 275, 0);
 				}
 
@@ -245,7 +260,7 @@ int iUzytyStol[4];							//tablica wykorzystanych stolow
 
 void ShutdownServer(struct klient clients[])
 {
-    // zamykamy niezamkniête socket'y klientów
+    // zamykamy niezamkniÄ™te socket'y klientÃ³w
     for (int i = 0; i < MAX_CLIENTS; i++) 
         if (clients[i].sock != INVALID_SOCKET)
         {
@@ -262,7 +277,7 @@ void ShutdownServer(struct klient clients[])
 
 int iIfFreeSlot(int active_players[])
 {
-int i;
+int i=0;
 for(int x=0;x<MAX_CLIENTS;x++)
 	if(active_players[x]) i++;
 return MAX_CLIENTS - i;
@@ -309,7 +324,7 @@ int __cdecl main(void)
   ///////////////////// // TUTAJ STARTUJEMY Z WINSOCKEM/////////////////////////////////
     iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (iResult != 0) {
-        printf("Wyst¹pi³ b³¹d modu³u WSAStartup : %d\n", iResult);
+        printf("WystÄ…piÅ‚ bÅ‚Ä…d moduÅ‚u WSAStartup : %d\n", iResult);
         return 1;
     }
 
@@ -322,24 +337,24 @@ int __cdecl main(void)
     // Najpierw uzupelniamy wlasne dane
     iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
     if ( iResult != 0 ) {
-        printf("B³ad getaddrinfo(): %d\n", iResult);
+        printf("BÅ‚ad getaddrinfo(): %d\n", iResult);
         WSACleanup();
         return 1;
     }
 
-    // Tworzymy gniazdo nas³uchuj¹ce:
+    // Tworzymy gniazdo nasÅ‚uchujÄ…ce:
     ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (ListenSocket == INVALID_SOCKET) {
-        printf("B³¹d gniazda: %ld\n", WSAGetLastError());
+        printf("BÅ‚Ä…d gniazda: %ld\n", WSAGetLastError());
         freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
 
-    // Nastêpnie je wi¹¿emy 
+    // NastÄ™pnie je wiÄ…Å¼emy 
     iResult = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
     if (iResult == SOCKET_ERROR) {
-        printf("B³¹d wi¹zania gniazda: %d\n", WSAGetLastError());
+        printf("BÅ‚Ä…d wiÄ…zania gniazda: %d\n", WSAGetLastError());
         freeaddrinfo(result);
         closesocket(ListenSocket);
         WSACleanup();
@@ -348,24 +363,24 @@ int __cdecl main(void)
 
     freeaddrinfo(result);
 	
-	// teraz oczekujemy na po³¹czenie z klientami// od tad nalezy rozwazac model aplikacji: stoliki
+	// teraz oczekujemy na poÅ‚Ä…czenie z klientami// od tad nalezy rozwazac model aplikacji: stoliki
 	while(SERVER_ON)
 	{
 		if (iIfFreeSlot(active_players)){
     iResult = listen(ListenSocket, SOMAXCONN);
     if (iResult == SOCKET_ERROR) {
-        printf("B³¹d nas³uchuj¹cego gniazda: %d\n", WSAGetLastError());
+        printf("BÅ‚Ä…d nasÅ‚uchujÄ…cego gniazda: %d\n", WSAGetLastError());
         closesocket(ListenSocket);
         WSACleanup();
         return 1;
     }
 	int free_slot = iFindFreeSlot(active_players);
 	active_players[free_slot]=1;
-    // Teraz akceptujemy transmisjê
+    // Teraz akceptujemy transmisjÄ™
 	gracze[free_slot].sock = accept(ListenSocket, NULL, NULL);
     if (gracze[active_players[free_slot]].sock == INVALID_SOCKET) {
         active_players[free_slot]=0;
-		printf("Nie powiod³a siê akceptacja transmisji: %d\n", WSAGetLastError());
+		printf("Nie powiodÅ‚a siÄ™ akceptacja transmisji: %d\n", WSAGetLastError());
         closesocket(ListenSocket);
         WSACleanup();
         return 1;
@@ -407,7 +422,7 @@ int __cdecl main(void)
 
 void sbMssgCLEAR(Buffer* x)
 {
-	for(int i=0;i<275;i++) x->cChat[i]=0;
+	for(int i=0;i<256;i++) x->cChat[i]=0;
 	x->ID=0;
 	x->ID_USR= 0;
 	for(int i=0;i<16;i++)	x->iKey[i] =0;
@@ -416,21 +431,69 @@ void sbMssgCLEAR(Buffer* x)
 
 void sConversion(Buffer *sbMssg, char cMessage[])
 {
-cMessage[0] = (char)sbMssg.ID;
-cMessage[1] = (char)sbMssg.ID_USR;
+cMessage[0] = (char)sbMssg->ID;
+cMessage[1] = (char)sbMssg->ID_USR;
 int k;
-for(k=2;k<18;k++) cMessage[k] = (char)sbMssg.iKey[k-2];
+for(k=2;k<18;k++) cMessage[k] = (char)sbMssg->iKey[k-2];
 
-for(k=18;k<275;k++) cMessage[k] = sbMssg.cChat[k-18];
+for(k=18;k<275;k++) cMessage[k] = sbMssg->cChat[k-18];
 
 }
 
 
-void rConversion(Buffer *sbMssg, char cMessage[])
+void rConversion(Buffer *sbMssg, char *cMessage)
 {
-sbMssg.ID =(int)cMessage[0];
-sbMssg.ID_USR =(int) cMessage[1];
+sbMssg->ID =(int)cMessage[0];
+sbMssg->ID_USR =(int) cMessage[1];
 int k;
-for(k=2;k<18;k++) sbMssg.iKey[k-2] = (int)cMessage[k];
-for(k=18;k<275;k++) sbMssg.cChat[k-18]=  cMessage[k];
+for(k=2;k<18;k++) sbMssg->iKey[k-2] = (int)cMessage[k];
+for(k=18;k<275;k++) sbMssg->cChat[k-18]=  cMessage[k];
+}
+
+
+
+short iGeneruIdGracza()
+{
+	int ID;
+	char buf[513];
+	FILE* X= fopen("login.txt", "rt");
+	while(fscanf(X, "%d", &ID)) {
+	fscanf(X, "%s", buf);
+	
+	}
+
+return 0;
+}
+
+
+int wybierzHaslo(Buffer* sbMssg)
+{
+
+return 0;
+}
+
+void cMessageForward(Buffer *sbMssg)
+{
+
+	return;
+}
+
+char* sWybierzHaslo(Buffer* sbMssg)
+{
+char napis[]=" ";
+
+return napis;
+}
+
+
+int iPorownajHaslo(Buffer *sbMssg, char napis[])
+{
+
+	return 0;
+}
+
+int iTranslate(Buffer* sbMssg)
+{
+
+return 0;
 }
